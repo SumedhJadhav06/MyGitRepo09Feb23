b868543f634b0c2228ab448f7328c94ad5aaaea1
Your Name <you@example.com>
2023-02-14 01:06:37 +0530

    My add function task completed

--- myaddfunctionpage.java
+++ myaddfunctionpage.java
@@ -1,3 +1,10 @@
 This is my add function page.
+<<<<<<< Updated upstream
 My manager work completed.
 
+=======
+1st day line java code.
+2nd day line java code.
+3rd day line java code.
+4th day line java code.
+5th day line java code.
